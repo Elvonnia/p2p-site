e32728208f3e6f5fc9533d8e6a4d5d3369d4c1ee
Elvonnia <kimberley.ohana@cegetel.net>
2016-11-15 10:22:01 +0100

    New rail project version

--- p2p/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-P2p::Application.config.secret_token = '6b9d649ff389e4afd9813784175e9fd9fe152a47baa60a1853778399253918fe580980716411c7bac17591d91b1d928f4a74e8a58e66b1c04ca86227300a3941'
