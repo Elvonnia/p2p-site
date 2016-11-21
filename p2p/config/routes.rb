56b1852a0d89e493797782bf35884e17e82f4c04
Krigan112 <darkhunt112@gmail.com>
2016-11-21 17:03:27 +0100

    ajout de vues chatroom/new, et modif des vues pour créer une nouvelle session. et pis modif des routes aussi. oklm

--- p2p/config/routes.rb
+++ p2p/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root "sessions#new"
 
+	get 'messages' => 'messages#index'
+	post 'messages' => 'messages#create'
+	get 'sessions' => 'sessions#new'
+
     mount ActionCable.server => '/cable' #enable websockets
 end
-
-get 'messages' => 'messages#index'
-post 'messages' => 'messages#create'
