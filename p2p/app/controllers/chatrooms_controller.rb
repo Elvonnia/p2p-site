ecbcc4e47b946415a8fbdc550da47ef9924e2e25
Elvonnia <kimberley.ohana@cegetel.net>
2016-11-22 11:35:57 +0100

    fix chatroom controller

--- p2p/app/controllers/chatrooms_controller.rb
+++ p2p/app/controllers/chatrooms_controller.rb
@@ -13,11 +13,7 @@ class ChatroomsController < ApplicationController
   #end
 
   def edit
-<<<<<<< HEAD
-    @chatroom = Chatroom.find_by!(chatroom_params)
-=======
     @chatroom = Chatroom.find_by(params[:id])
->>>>>>> 2a43956a134094b6029baa9cb46d87c40327ee38
   end
 
   def create
@@ -29,40 +25,28 @@ class ChatroomsController < ApplicationController
       end
     else
       respond_to do |format|
-<<<<<<< HEAD
         flash[:notice] = {error: ["A chatroom with this name already exists"]}
         format.html { redirect_to new_chatroom_path }
-=======
-        flash[:notice] = {error: ["a chatroom with this name already exists"]}
-        format.html { redirect_to chatrooms_path }
->>>>>>> 2a43956a134094b6029baa9cb46d87c40327ee38
+
         format.js { render template: 'chatrooms/chatroom_error.js.erb'} 
       end
     end
   end
 
   def update
-<<<<<<< HEAD
-    chatroom = Chatroom.find_by!(new_chatroom_path)
-=======
     chatroom = Chatroom.find(params[:id])
->>>>>>> 2a43956a134094b6029baa9cb46d87c40327ee38
     chatroom.update(chatroom_params)
     redirect_to chatroom
   end
 
   def show
-<<<<<<< HEAD
-    @chatroom = Chatroom.find_by!(chatroom_params)
-=======
     @chatroom = Chatroom.find(params[:id])
->>>>>>> 2a43956a134094b6029baa9cb46d87c40327ee38
     @message = Message.new
   end
 
   private
 
     def chatroom_params
-      params.require(:chatroom).find_by!(:name)
+      params.require(:chatroom).permit(:name)
     end
 end
